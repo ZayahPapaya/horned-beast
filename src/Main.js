c2da23d1d1e17d624c14d8299abbc5b10bd9860a
Aiden <hyena.shenanigans@gmail.com>
2022-04-23 16:59:03 -0700

    Added images, filled out components

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Snack from './Beast';
+import HornedBeast from './HornedBeast';
 
 class Main extends React.Component {
   render() {
     return (
       <>
-        <Beast title={'Beast1'} description={'A beast'} src={'IMGURL'} />
-        <Beast title={'Beast2'} description={'A beast'} src={'IMGURL'} />
-        <Beast title={'Beast3'}  description={'A beast'} src={'IMGURL'} />
-        <Beast title={'Beast4'}  description={'A beast'} src={'IMGURL'} />
+        <HornedBeast title={'Felidar Sovereign'} description={'It surveys the withering landscape, waiting for a victory only it can see.'} src={'https://media.magic.wizards.com/image_legacy_migration/mtg/images/daily/arcana/434_felidarsovereign.jpg'} />
+        <HornedBeast title={'Prowling Serpopard'} description={'The viziers serving Rhonas, the god of strength, maintain the menagerie of animals employed during his trial.'} src={'Prowling-Serpopard-Amonkhet-MtG-Art.jpg'} />
+        <HornedBeast title={'Darksteel Juggernaut'}  description={'One part unstoppable force, one part immovable object.'} src={'Darksteel_Juggernaut.jpg'} />
+        <HornedBeast title={'Darksteel Juggernaut'}  description={'One part unstoppable force, one part immovable object.'} src={'Into_the_Core.jpg'} />
       </>
     )
   }
