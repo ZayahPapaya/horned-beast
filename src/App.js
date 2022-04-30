3344fea90a85eef181c9c771fd24169841caf2da
Aiden <hyena.shenanigans@gmail.com>
2022-04-30 16:31:48 -0700

    Added black magic and also  modal

--- src/App.js
+++ src/App.js
@@ -4,13 +4,30 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import imageURLs from "./data.json";
+import SelectedBeast from "./SelectedBeast"
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      selectedBeast: {},
+    }
+  }
+  handleCloseModal = () => {
+    this.setState({showModal:false});
+  }
+  handleShowModal = (name) => {
+    const selectedBeast = imageURLs.find(value => value.title === name)
+    this.setState({selectedBeast: selectedBeast});
+    this.setState({showModal:true});
+  }
   render() {
     return (
       <div className="App">
         <Header title={"Pinterest but for Horned Beasts"}/>
-        <Main imageURLs={imageURLs} message={"Click on it lol"}/>
+        <SelectedBeast showModal={this.state.showModal} handleCloseModal={this.handleCloseModal} selectedBeast={this.state.selectedBeast}/>
+        <Main imageURLs={imageURLs} handleShowModal={this.handleShowModal}/>
         <Footer text={"☺"}/>
       </div>
     );
--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
+import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup'
 
 
 class Main extends React.Component {
   render() {
     return (
-        Object.values(this.props.imageURLs).map(value => {
-          return(
-            <><HornedBeast key={value._id} title={value.title} description={value.description} src={value.image_url}/></>
-          )
-        })
+      <>
+        {this.props.imageURLs.map(value => (
+          <HornedBeast key={value._id} title={value.title} description={value.description} src={value.image_url} handleShowModal={this.props.handleShowModal} />
+        ))}
+      </>
     )
   }
 }
