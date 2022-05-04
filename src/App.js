64161e2ae5b3395d30767e5bc9a2323fdeb31f9c
Aiden <hyena.shenanigans@gmail.com>
2022-05-03 19:50:49 -0700

    Baseline form set up, not finished(?)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Main from './Main';
 import Footer from './Footer';
 import imageURLs from "./data.json";
 import SelectedBeast from "./SelectedBeast"
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container'
 
 class App extends React.Component {
   constructor(props) {
@@ -12,6 +14,7 @@ class App extends React.Component {
     this.state = {
       showModal: false,
       selectedBeast: {},
+      userName: "User",
     }
   }
   handleCloseModal = () => {
@@ -22,14 +25,33 @@ class App extends React.Component {
     this.setState({selectedBeast: selectedBeast});
     this.setState({showModal:true});
   }
+  handleUserNameInput = (e) => {
+    e.preventDefault();
+    this.setState({userName: e.target.value});
+  }
   render() {
     return (
+      <>
       <div className="App">
         <Header title={"Pinterest but for Horned Beasts"}/>
+        <h2>Welcome {this.state.userName}</h2>
         <SelectedBeast showModal={this.state.showModal} handleCloseModal={this.handleCloseModal} selectedBeast={this.state.selectedBeast}/>
         <Main imageURLs={imageURLs} handleShowModal={this.handleShowModal}/>
         <Footer text={"☺"}/>
       </div>
+      <Container>
+        <Form>
+          <Form.Group controlId='bootstrapName'>
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" onInput={this.handleUserNameInput}/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Date of Birth</Form.Label>
+            <Form.Control type="date" />
+          </Form.Group>
+        </Form>
+      </Container>
+      </>
     );
   }
 }
